1754f711a7c7e4ce3a9c593e7489aedf5f8deed9
Leffebvre Gautier <Arakniro@desktop-gha2diu.home>
2020-11-13 02:10:56 +0100

    Récupération du nom des éléments depuis la DTD.

--- Partie-1/Gautier/src/main.c
+++ Partie-1/Gautier/src/main.c
@@ -4,11 +4,20 @@
 #include <string.h>
 #include <ctype.h>
 
+typedef struct Element Element;
+struct Element {
+	char *name;
+    Element *type;
+    int quantitator;
+};
+
+
 int checkFiles();							//Vérification de l'existence des fichiers.
 char *getFileContent(char *filePath);		//Récupération du contenu d'un fichier.
-int checkOpenEndDTD(char *dtd);					//Vérification du nombre d'élément dans la dtd
+int checkOpenEndDTD(char *dtd);				//Vérification du nombre d'élément dans la dtd.
 void removeMultipleSpaces(char *str);		//Remplace les espaces multiples par un espace.
 void replaceTabAndLineWithSpace(char *str);	//Remplace les \t et \n par un espace.
+Element *getElements(char *dtd);			//Récupération des éléments de la DTD sous la forme d'un tableau de structures "Element".
 
 int main(int argc, char **argv)
 {
@@ -27,10 +36,13 @@ int main(int argc, char **argv)
 	removeMultipleSpaces(xmlContent);
 	printf("DTD :\n%s\n", dtdContent);
 	printf("XML :\n%s\n", xmlContent);
-//	checkStartToEndDTD(dtdContent);
 	if (checkOpenEndDTD(dtdContent) == -1) {
 		printf("\nErreur\n");
 		return -1;
+	}
+	Element *elements = getElements(dtdContent);
+	for (int i = 0; elements[i].name[0] != '\0'; i = i + 1) {
+		printf("Element numéro %d : %s\n", i, elements[i].name);
 	}
 	return 0;
 }
@@ -78,10 +90,34 @@ void removeMultipleSpaces(char *str)
     *dest = '\0';
 }
 
+Element *getElements(char *dtd) {
+	Element *elements = malloc(sizeof(Element) * 100);
+	int indexElement = 0;
+	for (int i = 0; dtd[i + 10] != '\0'; i = i + 1) {
+		if (dtd[i] == '<' && dtd[i + 1] == '!' && dtd[i + 2] == 'E' && dtd[i + 3] == 'L' && dtd[i + 4] == 'E' && dtd[i + 5] == 'M' && dtd[i + 6] == 'E' && dtd[i + 7] == 'N' && dtd[i + 8] == 'T' && dtd[i + 9] == ' ') {	//On cherche les déclarations d'éléments : "<!ELEMENT ".
+			i = i + 10;
+			int j = 0;
+			elements[indexElement].name = malloc(sizeof(char) * 100);
+			while (dtd[i] != '(') {
+				elements[indexElement].name[j] = dtd[i];
+				j = j + 1;
+				i = i + 1;
+			}
+			elements[indexElement].name[j] = '\0';
+			indexElement = indexElement + 1;
+		}
+	}
+	elements[indexElement].name = malloc(sizeof(char) * 2);
+	elements[indexElement].name[0] = '\0';
+	return elements;
+}
 
 int checkOpenEndDTD(char *dtd) {
 	int nbOp = 0;
-	if (dtd[0] != '<' && dtd[1] != '!') {	//On vérifie que les 2 premiers caractères correspondent à l'ouverture d'une DTD.
+	if (strlen(dtd) < 10) {
+		return -1;
+	}
+	if (dtd[0] != '<' && dtd[1] != '!' && dtd[2] != 'D' && dtd[3] != 'O' && dtd[4] != 'C' && dtd[5] != 'T' && dtd[6] != 'Y' && dtd[7] != 'P' && dtd[8] != 'E') {	//On vérifie que le fichier commence bien par : "<DOCTYPE".
 		return -1;
 	}
 	for (int i = 0; dtd[i + 1] != '\0'; i = i + 1) {	//On compte les ouvertures : <! et fermeture : > afin de vérifier que le nombre d'ouverture corresponde au nombre de fermeture d'élément.

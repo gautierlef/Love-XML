4f5232326bc46334529b6a87f6644645654f85a2
Maathess <k.maathess@gmail.com>
2020-11-17 19:35:46 +0100

    TEST, je suis sous linux maintenant

---
+++ Partie-1/Maathess/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{/*
+    FILE *fp = fopen("texte.txt", "w");
+
+    if (fp == NULL)
+    {
+        printf("Le fichier texte.txt n'a pas pu être ouvert\n");
+        return EXIT_FAILURE;
+    }
+    if (fputc('C', fp) == EOF)
+    {
+        printf("Erreur lors de l'écriture d'un caractère\n");
+        return EXIT_FAILURE;
+    }
+    if (fclose(fp) == EOF)
+    {
+        printf("Erreur lors de la fermeture du flux\n");
+        return EXIT_FAILURE;        
+    }*/
+
+	printf("Je suis passé sur linux");
+
+    return 0;
+}
